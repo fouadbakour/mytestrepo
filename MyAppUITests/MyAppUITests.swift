609bb888b6a14d8860051f19f8afc4a45a0adc0e
Shyreen <owner1@macbook-pro-2.lan>
2016-05-05 10:17:28 +0300

    snap shot 2 views test

--- MyAppUITests/MyAppUITests.swift
+++ MyAppUITests/MyAppUITests.swift
@@ -19,13 +19,6 @@ class MyAppUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-        
-        let app = XCUIApplication()
-        app.launchEnvironment = [ "UITest": "1" ]
-        setupSnapshot(app)
-        app.launch()
-        
-        snapshot("0Launch")
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -36,6 +29,19 @@ class MyAppUITests: XCTestCase {
     }
     
     func testExample() {
+        
+        let app = XCUIApplication()
+        app.launchEnvironment = [ "UITest": "1" ]
+        setupSnapshot(app)
+        app.launch()
+        
+        snapshot("0FirstView")
+        
+        // Tap  button
+        app.buttons["My Button"].tap()
+        
+        snapshot("1SecondView")
+
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
